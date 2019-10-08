c291317cafdab399a41a0159ff1ed997231a9402
Katrina Fuller <trinafuller@att.net>
2019-10-08 13:54:15 -0600

    started Ticket Model

--- Program/Models/Ticket.cs
+++ Program/Models/Ticket.cs
@@ -1,7 +1,21 @@
+using csharp_ticketTracker.Interfaces;
+
 namespace csharp_ticketTracker.Models
 {
-  public class Ticket
+  public class Ticket : ITicket
   {
 
+    public string Title { get; set; }
+    public string Description { get; set; }
+
+    public string GetTemplate()
+    {
+
+    }
+    public Ticket(string title, string description)
+    {
+      Title = title;
+      Description = description;
+    }
   }
 }
